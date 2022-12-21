38a73647ed7806325004759c6d55dc3025e83823
akii1998 <110331686+akii1998@users.noreply.github.com>
2022-12-22 01:58:54 +0530

    Basic Structureof file complete

--- src/App.js
+++ src/App.js
@@ -3,26 +3,32 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Settings from "./pages/Settings";
 import Questions from "./pages/Questions";
 import FinalScreen from "./pages/FinalScreen";
+import { Box, Container, Typography } from "@mui/material";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Settings/>,
+    element: <Settings />,
   },
   {
     path: "/questions",
     element: <Questions />,
   },
   {
-    path:'/score',
-    element:<FinalScreen/>
-  }
+    path: "/score",
+    element: <FinalScreen />,
+  },
 ]);
 
 function App() {
   return (
     <div className="App">
-      <RouterProvider router={router} />
+      <Container maxWidth="sm">
+        <Box textAlign={"center"} mt={5}>
+        <Typography variant="h2" fontWeight={"bold"}>Quiz App</Typography>
+          <RouterProvider router={router} />
+        </Box>
+      </Container>
     </div>
   );
 }
--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -1,8 +1,26 @@
+import { Box, Button } from '@mui/material'
 import React from 'react'
+import SelectField from '../components/SelectField'
+import TextFieldComp from '../components/TextFieldComp'
+
 
 const Settings = () => {
+    const handleSubmit=(e)=>{
+        e.preventDefault();
+
+    }
   return (
-    <div>Settings</div>
+   <form onSubmit={handleSubmit}>
+    <SelectField label="Category"/>
+    <SelectField label="Difficulty"/>
+    <SelectField label="Type"/>
+    <TextFieldComp/>
+    <Box mt={3} width="100%">
+    <Button fullWidth variant='contained' type="submit">
+        Get Started
+    </Button>
+    </Box>
+   </form>
   )
 }
 
