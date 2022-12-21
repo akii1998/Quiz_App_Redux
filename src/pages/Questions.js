56cdc896aced73f7667c22b92ba4e143bfa00590
akii1998 <110331686+akii1998@users.noreply.github.com>
2022-12-22 03:16:42 +0530

    redux

--- src/pages/Questions.js
+++ src/pages/Questions.js
@@ -1,7 +1,23 @@
 import { Box, Button, Typography } from "@mui/material";
 import React from "react";
+import { useSelector } from "react-redux";
+import useAxios from "../Hooks/useAxios";
 
 const Questions = () => {
+
+   const {
+    question_category,
+    question_difficulty,
+    question_type,
+    amount_of_question,
+   }=useSelector(state=>state)
+   console.log(amount_of_question)
+
+
+
+    const apiUrl=`/api.php?amount=10`
+    const  {response ,error ,loading} =useAxios({url:apiUrl})
+    console.log(response)
   return (
     <Box>
       <Typography variant="h4">Questions 1</Typography>
